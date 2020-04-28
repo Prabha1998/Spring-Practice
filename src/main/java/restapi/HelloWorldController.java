253c808448ab51f9d51ffaa930412482556a5b82
PrabhaRashmi <Prabha1998>
2020-04-28 16:34:09 +0530

    First Commit

---
+++ src/main/java/restapi/HelloWorldController.java
@@ -0,0 +1,14 @@
+package restapi;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+//Controller
+@RestController
+public class HelloWorldController
+{
+    //using get method and hello-world as URI
+    @GetMapping(path="/hello-world")
+    public String helloWorld()
+    {
+        return "Hello World";
+    }
+}
